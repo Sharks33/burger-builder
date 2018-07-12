27be5452f7ba67ce68d63df306373c0d774d6cee
Thomas Eggenberger <meshugg83@yahoo.com>
2018-07-12 09:43:21 -0600

    add project source files

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import classes from './Burger';
+import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = () => {
+const burger = ( props ) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
+        .reduce( ( arr, el ) => {
+            return arr.concat( el );
+        }, [] );
+    // console.log(transformedIngredients);
+    if ( transformedIngredients.length === 0 ) {
+        transformedIngredients = <p> Please add some ingredients! </p>
+    }
+
     return (
         <div className={classes.Burger}> 
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="bacon"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     )
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
-    render() {
+    render () {
         let ingredient = null;
 
         switch ( this.props.type ) {
@@ -13,8 +14,8 @@ class BurgerIngredient extends Component {
             case ( 'bread-top' ):
                 ingredient = (
                     <div className={classes.BreadTop}>
-                        <div className={classes.Seeds1}></div>;
-                        <div className={classes.Seeds2}></div>;
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
                     </div>
                 );
                 break;
@@ -24,19 +25,19 @@ class BurgerIngredient extends Component {
             case ( 'cheese' ):
                 ingredient = <div className={classes.Cheese}></div>;
                 break;
-            case ( 'salad' ):
-                ingredient = <div className={classes.Salad}></div>;
-                break;
             case ( 'bacon' ):
+                ingredient = <div className={classes.Bacon}></div>;
+                break;
+            case ( 'salad' ):
                 ingredient = <div className={classes.Salad}></div>;
                 break;
             default:
                 ingredient = null;
         }
+
         return ingredient;
-        
     }
-};
+}
 
 BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
